856c2d0bda7481e762644c681d9eb0952562eff7
Nickgrand <2807224231@qq.com>
2023-04-05 17:45:48 +0800

    增加mybatis-generator生成的文件

---
+++ src/main/java/com/qyf/model/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.qyf.model.dao;
+
+import com.qyf.model.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
