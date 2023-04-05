d7134c997da7d05250d3213aa28c04c69675869a
Nickgrand <2807224231@qq.com>
2023-04-05 18:27:20 +0800

    更新文件路径

--- src/main/java/com/qyf/mall/MallApplication.java
+++ src/main/java/com/qyf/mall/MallApplication.java
@@ -1,15 +1,1 @@
-package com.qyf.mall;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-@MapperScan("com.qyf.model.dao")
-public class MallApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MallApplication.class, args);
-    }
-
-}
+package com.qyf.mall;import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication@MapperScan("com.qyf.mall.model.dao")public class MallApplication {    public static void main(String[] args) {        SpringApplication.run(MallApplication.class, args);    }}
