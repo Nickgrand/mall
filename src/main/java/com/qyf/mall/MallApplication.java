9c0030596cf49065eae9e9ff2ba09b2f667a9309
Nickgrand <2807224231@qq.com>
2023-04-05 18:04:09 +0800

    更新开发分支，更新注释文件，修复找不到mapper的告警

--- src/main/java/com/qyf/mall/MallApplication.java
+++ src/main/java/com/qyf/mall/MallApplication.java
@@ -1,9 +1,11 @@
 package com.qyf.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qyf.model.dao")
 public class MallApplication {
 
     public static void main(String[] args) {
