7d661632201714734a0d7c8123cfc3a308a0044f
Nickgrand <2807224231@qq.com>
2023-04-05 17:12:31 +0800

    init mall project

---
+++ src/test/java/com/qyf/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qyf.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
